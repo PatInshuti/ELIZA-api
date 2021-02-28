5eeb87a7746147400f8ed427fafad82df78750bf
PatInshuti <pim219@nyu.edu>
2021-02-28 22:59:32 +0400

    .

---
+++ server.js
@@ -0,0 +1,32 @@
+
+const express = require('express');
+const Eliza = require('eliza-as-promised');
+const app = express();
+const port = 3000
+
+ 
+var eliza = new Eliza(); 
+
+app.get('/:prompt', (req, res) => {
+    
+    const prompt = req.params.prompt;
+
+    eliza.getResponse(prompt).then((response) => {
+        if (response.reply) {
+            // console.log('>> ' + response.reply);
+            
+            res.json({"response":response})
+        }
+        if (response.final) {
+            // console.log('>>> ' + response.final);
+            res.json({"response":response.final})
+        }
+    });
+
+    console.log(eliza.getResponse())
+
+})
+
+app.listen(port, () => {
+    console.log(`ELIZA Server listening on ${port}`)
+})
